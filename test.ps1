17e4180de79de1b21e90f791937eedd80810015a
Dennis Westerman <71754369+denniswesterman@users.noreply.github.com>
2024-09-30 23:02:28 +0200

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,3 +1,1 @@
-#region Initialize
-$Transcript = "$((Get-Date).ToString('yyyy-MM-dd-HHmmss'))-$ScriptName.log"
-$null = Start-Transcript -Path (Join-Path "$env:SystemRoot\Temp" $Transcript) -ErrorAction Ignore
+write-output "test"

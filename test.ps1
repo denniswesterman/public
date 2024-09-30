c66733068610012a3146e12396505cd2623ca616
Dennis Westerman <71754369+denniswesterman@users.noreply.github.com>
2024-09-30 23:00:53 +0200

    Create test.ps1

---
+++ test.ps1
@@ -0,0 +1,3 @@
+#region Initialize
+$Transcript = "$((Get-Date).ToString('yyyy-MM-dd-HHmmss'))-$ScriptName.log"
+$null = Start-Transcript -Path (Join-Path "$env:SystemRoot\Temp" $Transcript) -ErrorAction Ignore
